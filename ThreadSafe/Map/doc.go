db60036586305dc8fae30c5ff999707bc4da2f47
ivan-kostko <kostko_i@yahoo.com>
2016-04-24 18:22:47 +0200

    ThreadSafe\Map doc fixes

--- ThreadSafe/Map/doc.go
+++ ThreadSafe/Map/doc.go
@@ -12,10 +12,17 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+/*
+Import
+    "github.com/ivan-kostko/ThreadSafe/Map"
+
+Install
+    go get github.com/ivan-kostko/ThreadSafe/Map
+
+Description
+    The ThreadSafeMap package represents light weight and simple API for thread safe map.
+
+    The functionality is very limited to make it clean and robust. In case of operating on big amounts of data or need of extended functionality - consider to use https://github.com/streamrail/concurrent-map
+*/
 package Map
 
-/*
-The ThreadSafeMap package represents light weight and simple API for thread safe map.
-
-The functionality is very limited to make it clean and robust. In case of operating on big amounts of data or need of extended functionality - consider to use https://github.com/streamrail/concurrent-map
-*/
