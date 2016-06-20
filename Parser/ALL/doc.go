501846e3b5b10bc7f507f13e5ba035c84c1f271f
ivan-kostko <kostko_i@yahoo.com>
2016-06-20 06:42:25 +0200

    Parser ALL package

---
+++ Parser/ALL/doc.go
@@ -0,0 +1,31 @@
+//   Copyright (c) 2016 Ivan A Kostko (github.com/ivan-kostko)
+
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+
+//       http://www.apache.org/licenses/LICENSE-2.0
+
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+/*
+Install
+
+    go get "github.com/ivan-kostko/GoLibs/Parser/ALL"
+
+Import
+
+    "github.com/ivan-kostko/GoLibs/Parser"
+    _ "github.com/ivan-kostko/GoLibs/Parser/ALL"
+
+Description
+
+The Parser/ALL package represents assembly of wrappers for ALL parsing library(es).
+It is adapted to interfaces defined at github.com/ivan-kostko/GoLibs/Parser and registers all(included) underlying implementations at Parser.
+
+*/
+package all
